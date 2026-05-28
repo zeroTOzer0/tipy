821aa165e21df53532fe3487c6866f3c75ba2f4e
zerotozero <zerotozero@tutamail.com>
2026-05-28 23:26:15 +0100

    Rewrite internet checksum function; remove numba dependency

--- tipy/lib/csum.py
+++ tipy/lib/csum.py
@@ -1,22 +1,30 @@
-from numba import njit
+from struct import unpack_from
 
-@njit(fastmath=True)
 def inet_csum(data: memoryview, pseudo_sum: int=0):
     """ Compute internet checksum """
     s = pseudo_sum
     n = len(data)
-
     i = 0
+
+    num_qwords = n // 8
+
+    # Sum all full 64-bit words at once
+    s += sum(
+        unpack_from(f'!{n // 8}Q', data[:num_qwords * 8])
+    )
+    i += (num_qwords * 8)
+
+    # Process remaining data in 16-bit (2-byte) chunks
     while i + 1 < n:
-        s += (data[i] << 8) | data[i + 1]
-        s = (s & 0xFFFF) + (s >> 16)
+        s += (data[i] + data[i+1])
         i += 2
 
-    if n & 1:
-        s += (data[n - 1] << 8)
-        s = (s & 0xFFFF) + (s >> 16)
+    # If there is a leftover single byte, pad it
+    if n % 2:
+        s += (data[-1] << 8)
 
-    s = (s & 0xFFFF) + (s >> 16)
-    s = (s & 0xFFFF) + (s >> 16)
+    # Fold 32-bit/64-bit sum into 16-bit by adding carries
+    while s >> 16:
+        s = (s >> 16) + (s & 0xFFFF)
 
-    return (~s) & 0xFFFF
+    return ~s & 0xFFFF
